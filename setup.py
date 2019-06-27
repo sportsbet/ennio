4d7beb1c6953f4753470cb579c1d214cf965b966
Kai Xia <kaix+github@fastmail.com>
2019-06-27 10:35:49 +1000

    add long description and trove classifiers.
    Signed-off-by: Kai Xia <kaix+github@fastmail.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import find_packages, setup
 
 
-VERSION = '0.1.1'
+VERSION = "0.1.2"
 
+
+with open("README.md") as fobj:
+    long_description = fobj.read()
 
 if __name__ == "__main__":
     setup(
@@ -12,7 +15,17 @@ if __name__ == "__main__":
         author_email="engineering@sportsbet.com.au",
         license="Apache-2.0",
         url="https://github.com/sportsbet/ennio",
-        description="Interstack orchestration tool for AWS CloudFormation",
+        description="interstack orchestration framework for AWS CloudFormation",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
         packages=find_packages(),
         install_requires=["PyYAML", "boto3", "jinja2"],
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+        ],
     )
